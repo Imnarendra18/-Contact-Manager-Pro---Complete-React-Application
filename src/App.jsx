fa9dbc8211b4fd0095e401058f998ba2e316b12f
Imnarendra18 <narendra23233@gamil.com>
2026-01-30 14:57:13 +0530

    Initial commit - Contact Manager React App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ContactManager from './CE30012026/ContactManager.jsx';
+
+function App() {
+  return (
+    <>
+      <ContactManager/>
+    </>
+  );
+}
+
+export default App;
